46878e4691da91e8683bc557cff7f7d57a7a7d48
Dominic McPhee <dominic.mcphee@gmail.com>
2013-02-28 22:01:04 -0500

    - Adding flags for currencies - Overall UI improvements

--- src/com/example/boccurrencyconverter/MainActivity.java
+++ src/com/example/boccurrencyconverter/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.boccurrencyconverter;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -15,15 +21,28 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.PopupMenu.OnMenuItemClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 @SuppressLint("NewApi")
 public class MainActivity extends Activity {
 	JSONArray rates;
+	public static String PACKAGE_NAME;
+	List ratesList;
+	
+	ListView fromList;
+	ListView toList;
+	String[] ratesArray;
 	
 	BigDecimal fromAmount;
 	BigDecimal toAmount;
@@ -36,17 +55,23 @@ public class MainActivity extends Activity {
 	int fromSelected;
 	int toSelected;
 	
+	Dialog fromDialog;
+	Dialog toDialog;
+	
+	RateAdapter fromAdapter;
+	RateAdapter toAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		setContentView(R.layout.activity_main);
-
+		
+		PACKAGE_NAME = getApplicationContext().getPackageName();
+		
 		fromSelected = 0;
 		toSelected = 1;
 
-		
 		View view = this.findViewById(android.R.id.content);
 		final Activity activity = this;
 		
@@ -114,6 +139,8 @@ public class MainActivity extends Activity {
         });
 
 		JSONClient client = new JSONClient(this, l);
+		
+		// Set url with sort json encoded
 		String url = "http://exchng.ca/rates?%7b%22$sort%22:%7b%22order%22:1%7d%7d";
 		client.execute(url);
 	}
@@ -163,10 +190,33 @@ public class MainActivity extends Activity {
 	}
 	
 	public void swapCurrencies(View v) {
-		// Swap 
+		// Back up flag
+		ImageView fromFlag = (ImageView) findViewById(R.id.fromImage);
+		ImageView toFlag = (ImageView) findViewById(R.id.toImage);
+		
+		// Swap
+		try {
+			String file = rates.getJSONObject(fromSelected).getString("shortcode").toString().toLowerCase() + "_flag";
+			int resID = getResources().getIdentifier(file , "drawable", MainActivity.PACKAGE_NAME);
+			toFlag.setImageResource(resID);
+			file = rates.getJSONObject(toSelected).getString("shortcode").toString().toLowerCase() + "_flag";
+			resID = getResources().getIdentifier(file , "drawable", MainActivity.PACKAGE_NAME);
+			fromFlag.setImageResource(resID);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		int tempSelected = toSelected;
 		toSelected = fromSelected;
 		fromSelected = tempSelected;
+		
+		if (fromList != null) {
+			fromList.setItemChecked(fromSelected, true);
+		}
+		if (toList != null) {
+			toList.setItemChecked(toSelected, true);
+		}
 		
 		Button selectFromButton = (Button) findViewById(R.id.selectConvertFrom);
 		String labelTo = (String) selectFromButton.getText();
@@ -181,65 +231,126 @@ public class MainActivity extends Activity {
 	}
 	
 	public void showSelectFrom(View v) {
-	    PopupMenu popup = new PopupMenu(this, v);
-	    
-	    // This activity implements OnMenuItemClickListener
-	    popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-
-	        @Override
-	        public boolean onMenuItemClick(MenuItem item) {
-	            MainActivity.this.selectNewFrom(item);
-	            return true;
+		if (fromDialog == null) {
+			// custom dialog
+			fromDialog = new Dialog(getBaseContext());
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Select Currency");
+			builder.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog,int id) {
+                    dialog.dismiss();
+                }
+            });
+			
+			
+			fromList = new ListView(this);
+			
+			ratesList = new ArrayList(rates.length());
+			fromList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+			
+	        // Fill the songs array by using a for loop
+	        for(int i=0; i < rates.length(); i++){
+	        	try {
+	        		ratesList.add(new Currency(rates.getJSONObject(i).getString("label").toString(), rates.getJSONObject(i).getString("shortcode").toString()));
+	        	} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	        }
-	    });
-	    
-	    for (int i=0; i < rates.length(); i++) {
-	    	try {
-	    		int order = Integer.parseInt(rates.getJSONObject(i).getString("order").toString());
-				popup.getMenu().add(0, i, order, rates.getJSONObject(i).getString("label").toString());
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    }
+	        
+	        fromAdapter = new RateAdapter(this, R.layout.list_row, ratesList, getResources(), fromSelected);
 
-	    popup.show();
+			fromList.setAdapter(fromAdapter);
+			
+			fromList.setItemChecked(fromSelected, true);
+			
+			// Defining the item click listener for listView
+	        OnItemClickListener itemClickListener = new OnItemClickListener() {
+	            @Override
+	            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+	            	MainActivity.this.selectNewFrom(position);
+	            	fromDialog.dismiss();
+	             }
+	        };
+	        
+	        // Setting the item click listener for listView
+	        fromList.setOnItemClickListener(itemClickListener);
+	        fromList.setBackgroundColor(R.color.almostBlack);
+	        
+			builder.setView(fromList);
+			fromDialog = builder.create();
+		}
+ 
+		fromDialog.show();
 	}
 	
 	public void showSelectTo(View v) {
-	    PopupMenu popup = new PopupMenu(this, v);
-	    
-	    // This activity implements OnMenuItemClickListener
-	    popup.setOnMenuItemClickListener(new OnMenuItemClickListener() {
-
-	        @Override
-	        public boolean onMenuItemClick(MenuItem item) {
-	            MainActivity.this.selectNewTo(item);
-	            return true;
+		if (toDialog == null) {
+			// custom dialog
+			toDialog = new Dialog(getBaseContext());
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Select Currency");
+			builder.setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog,int id) {
+                    dialog.dismiss();
+                }
+            });
+			
+			toList = new ListView(this);
+			
+			ratesList = new ArrayList(rates.length());
+			toList.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+			
+	        // Fill the songs array by using a for loop
+	        for(int i=0; i < rates.length(); i++){
+	        	try {
+	        		ratesList.add(new Currency(rates.getJSONObject(i).getString("label").toString(), rates.getJSONObject(i).getString("shortcode").toString()));
+	        	} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 	        }
-	    });
-	    
-	    for (int i=0; i < rates.length(); i++) {
-	    	try {
-	    		int order = Integer.parseInt(rates.getJSONObject(i).getString("order").toString());
-				popup.getMenu().add(0, i, order, rates.getJSONObject(i).getString("label").toString());
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	    }
+	        
+	        toAdapter = new RateAdapter(this, R.layout.list_row, ratesList, getResources(), toSelected);
 
-	    popup.show();
+			toList.setAdapter(toAdapter);
+			toList.setItemChecked(toSelected, true);
+			
+			// Defining the item click listener for listView
+	        OnItemClickListener itemClickListener = new OnItemClickListener() {
+	            @Override
+	            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
+	            	MainActivity.this.selectNewTo(position);
+	            	toDialog.dismiss();
+	            }
+	        };
+	        
+	        // Setting the item click listener for listView
+	        toList.setOnItemClickListener(itemClickListener);
+	        toList.setBackgroundColor(R.color.almostBlack);
+	        
+			builder.setView(toList);
+			toDialog = builder.create();
+		}
+		
+		toDialog.show();
 	}
 	
-	public boolean selectNewFrom(MenuItem item) {
-	    fromSelected = item.getItemId();
+	public boolean selectNewFrom(int selected) {
+	    fromSelected = selected;
+	    fromList.setItemChecked(fromSelected, true);
+	    
+	    fromAdapter.setSelected(fromSelected);
 	    
 	    Button button = (Button) findViewById(R.id.selectConvertFrom);
-	    button.setText(item.getTitle());
+	    ImageView flag = (ImageView) findViewById(R.id.fromImage);
 	    
 	    try {
+	    	button.setText(rates.getJSONObject(fromSelected).getString("label").toString());
 			fromRate = new BigDecimal(rates.getJSONObject(fromSelected).getString("rate").toString());
+			String file = rates.getJSONObject(fromSelected).getString("shortcode").toString().toLowerCase() + "_flag";
+			int resID = getResources().getIdentifier(file , "drawable", MainActivity.PACKAGE_NAME);
+			flag.setImageResource(resID);
 			calculateExchangeRate();
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
@@ -249,14 +360,21 @@ public class MainActivity extends Activity {
 	    return true;
 	}
 	
-	public boolean selectNewTo(MenuItem item) {
-	    toSelected = item.getItemId();
+	public boolean selectNewTo(int selected) {
+	    toSelected = selected;
+	    toList.setItemChecked(toSelected, true);
+	    
+	    toAdapter.setSelected(toSelected);
 	    
 	    Button button = (Button) findViewById(R.id.selectConvertTo);
-	    button.setText(item.getTitle());
+	    ImageView flag = (ImageView) findViewById(R.id.toImage);
 	    
 	    try {
-			toRate = new BigDecimal(rates.getJSONObject(toSelected).getString("rate").toString());
+	    	button.setText(rates.getJSONObject(toSelected).getString("label").toString());
+			fromRate = new BigDecimal(rates.getJSONObject(toSelected).getString("rate").toString());
+			String file = rates.getJSONObject(toSelected).getString("shortcode").toString().toLowerCase() + "_flag";
+			int resID = getResources().getIdentifier(file , "drawable", MainActivity.PACKAGE_NAME);
+			flag.setImageResource(resID);
 			calculateExchangeRate();
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
